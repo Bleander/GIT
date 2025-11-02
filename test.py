c1605a9c937712dd757bcbb680471e518bf9835a
Bleander <bleanderono7@students.tukenya.ac.ke>
2025-11-02 18:22:20 +0300

    Update

--- test.py
+++ test.py
@@ -11,8 +11,6 @@ print("Byee Mate")
 print("Byee Friend")
 print("Byee Pal")
 print("Byee Mate")
-
-i love God
-i love myself
-i love my family
-i love my friends
+print("Byee Friend")
+print("Byee Pal")
+print("Byee Mate")
